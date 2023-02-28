6d6fff3437014adf8035db74b81087edd9181693
Gabrielfernandezms <125490585+Gabrielfernandezms@users.noreply.github.com>
2023-02-28 18:23:40 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function mostrardatospersonales(datospersonales, experiencialaboral){
+document.getElementById('datospersonales').style.visibility='visible';
+document.getElementById('experiencialaboral').style.visibility='hidden';
+}
+
+function mostrarexperiencialaboral(experiencialaboral, datospersonales){
+    document.getElementById('experiencialaboral').style.visibility='visible';
+    document.getElementById('datospersonales').style.visibility='hidden';
+    
+
+    }
